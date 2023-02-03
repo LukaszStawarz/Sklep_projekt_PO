f6ff12e20498228d7d19950f78c828529dfc8e2b
ŁukaszStawarz <lukasz9631@gmail.com>
2023-02-03 15:25:57 +0100

    test

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -4,35 +4,65 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class AppTest
     extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+    public void testDodajProdukt() {
+        Produkty produkty24 = new Produkty(0,5,5,"Odświeżacz powietrza",1,13.99);
+        ProduktyCRUD produktyCRUD = new ProduktyCRUD();
+        int prodId = produktyCRUD.saveProdukty(produkty24);
+        assertNotNull(prodId);
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    public void testGetProduktList() {
+        ProduktyCRUD produktyCRUD = new ProduktyCRUD();
+        List<Produkty> list =produktyCRUD.getProdukty();
+        assertTrue(!list.isEmpty());
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    public void testLoginAndRegister() {
+        Klient_logowanie klient_logowanie = new Klient_logowanie("test123", "test213");
+        Klient_logowanieCRUD klient_logowanieCRUD = new Klient_logowanieCRUD();
+        int id =  klient_logowanieCRUD.saveKlient_logowanie(klient_logowanie);
+        assertNotNull(id);
+
+        //check if user can login
+        Klient_logowanie klient = klient_logowanieCRUD.checkIfKlientExists("test123", "test213");
+        assertNotNull(klient);
     }
+
+    public void testPromocje(){
+        PromocjeCRUD promocjeCRUD = new PromocjeCRUD();
+        List<Promocje> list = promocjeCRUD.getPromocje();
+        assertNotNull(list.isEmpty());
+    }
+    public void testZamówienia(){
+        ZamowieniaCRUD zamowieniaCRUD = new ZamowieniaCRUD();
+        List<Zamowienia> list = zamowieniaCRUD.getZamowienia();
+        assertNotNull(zamowieniaCRUD);
+    }
+    public void testProducent() {
+        ProducentCRUD producentCRUD = new ProducentCRUD();
+        List<Producent> list = producentCRUD.getProducent();
+        assertNotNull(producentCRUD);
+    }
+    public void testPracownik(){
+        PracownicyCRUD pracownicyCRUD = new PracownicyCRUD();
+        List<Pracownicy> list = pracownicyCRUD.getPracownicy();
+        assertNotNull(pracownicyCRUD);
+    }
+    public void testKategorie(){
+        Kategoria_produktuCRUD kategoria_produktuCRUD = new Kategoria_produktuCRUD();
+        List<Kategoria_produktu> list = kategoria_produktuCRUD.getKategoria_produktu();
+        assertNotNull(kategoria_produktuCRUD);
+    }
+
 }
+
+
